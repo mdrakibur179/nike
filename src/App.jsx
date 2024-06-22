074f7f0b446bc06da1bad2ebc0302a943504b0c6
Md Rakibur Rahman <mdrakibur179@gmail.com>
2024-06-22 16:38:10 +0600

    fixed components name and errors

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import React from "react";
 import "./styles/output.css";
 
@@ -5,14 +6,14 @@ const App = () => {
   return (
     <main className="relative">
       Nav
-      <section className="xl:padding-1 wide:padding-r padding-b">Hero</section>
-      <section className="padding">Popular Product</section>
-      <section className="padding">Super Quality</section>
-      <section className="padding-s py-10">Services</section>
-      <section className="padding">Special Offer</section>
-      <section className="padding bg-pale-blue">Customer Reviews</section>
-      <section className="padding-x sm:py-32 py-16 w-full">Subscribe</section>
-      <section className="bg-black padding-x padding-t pb-8">Footer</section>
+      <section className="xl:padding-1 wide:padding-r padding-b">hero</section>
+      <section className="padding">popularProduct</section>
+      <section className="padding">superQuality</section>
+      <section className="padding-s py-10">services</section>
+      <section className="padding">specialOffer</section>
+      <section className="padding bg-pale-blue">customerReviews</section>
+      <section className="padding-x sm:py-32 py-16 w-full">subscribe</section>
+      <section className="bg-black padding-x padding-t pb-8">footer</section>
     </main>
   );
 };
