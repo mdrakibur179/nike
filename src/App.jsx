6654b528e613a238f0fcbf785e0a5bc30dd7321a
Md Rakibur Rahman <mdrakibur179@gmail.com>
2024-06-22 17:07:02 +0600

    added all the components manually

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,34 @@
-/* eslint-disable no-unused-vars */
-import React from "react";
 import "./styles/output.css";
+import { Hero, PopularProducts, SuperQuality, Services, SpecialOffer, CustomerReviews, Subscribe, Footer } from "./sections";
 
 const App = () => {
   return (
     <main className="relative">
       Nav
-      <section className="xl:padding-1 wide:padding-r padding-b">hero</section>
-      <section className="padding">popularProduct</section>
-      <section className="padding">superQuality</section>
-      <section className="padding-s py-10">services</section>
-      <section className="padding">specialOffer</section>
-      <section className="padding bg-pale-blue">customerReviews</section>
-      <section className="padding-x sm:py-32 py-16 w-full">subscribe</section>
-      <section className="bg-black padding-x padding-t pb-8">footer</section>
+      <section className="xl:padding-1 wide:padding-r padding-b">
+        <Hero />
+      </section>
+      <section className="padding">
+        <PopularProducts />
+      </section>
+      <section className="padding">
+        <SuperQuality />
+      </section>
+      <section className="padding-s py-10">
+        <Services />
+      </section>
+      <section className="padding">
+        <SpecialOffer />
+      </section>
+      <section className="padding bg-pale-blue">
+        <CustomerReviews />
+      </section>
+      <section className="padding-x sm:py-32 py-16 w-full">
+        <Subscribe />
+      </section>
+      <section className="bg-black padding-x padding-t pb-8">
+        <Footer />
+      </section>
     </main>
   );
 };
