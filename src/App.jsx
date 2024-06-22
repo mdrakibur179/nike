c71b0352ba5808da4b434acfce330f3837f80160
mdrakibur179 <mdrakibur179@gmail.com>
2024-06-22 16:12:32 +0600

    added static sections in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
+import React from "react";
 import "./styles/output.css";
 
-export default function App() {
+const App = () => {
   return (
-    <h1 className="text-5xl font-bold text-center mt-4">
-      Successfully Created New Branch
-    </h1>
+    <main className="relative">
+      Nav
+      <section className="xl:padding-1 wide:padding-r padding-b">Hero</section>
+      <section className="padding">Popular Product</section>
+      <section className="padding">Super Quality</section>
+      <section className="padding-s py-10">Services</section>
+      <section className="padding">Special Offer</section>
+      <section className="padding bg-pale-blue">Customer Reviews</section>
+      <section className="padding-x sm:py-32 py-16 w-full">Subscribe</section>
+      <section className="bg-black padding-x padding-t pb-8">Footer</section>
+    </main>
   );
-}
+};
+
+export default App;
