8d7d01a7282efe72f389d6ac41e5258a97094fd1
Md Rakibur Rahman <mdrakibur179@gmail.com>
2024-06-23 09:43:50 +0600

    added nav items in navigation bar

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,13 +1,33 @@
-import headerLogo from "../assets/images/header-logo.svg";
-// import hamburger from "../assets/icons/hambuger.svg";
+import { headerLogo } from "../assets/images/";
+import { navLinks } from "../constants";
+import { hamburger } from "../assets/icons/";
 
 const Nav = () => {
   return (
-    <header>
-      <nav>
+    <header className="padding-x py-8 absolute z10 w-full">
+      <nav className="flex justify-between items-center max-container">
         <a href="#">
           <img src={headerLogo} alt="headerLogo" width={130} height={29} />
         </a>
+
+        <ul className="flex flex-1 justify-center items-center gap-16 max-lg:hidden">
+          {navLinks.map((item) => (
+            <li key={item.label}>
+              <a
+                href={item.href}
+                className="font-montserrat leading-normal text-lg text-slate-gray"
+              >
+                {item.label}
+              </a>
+            </li>
+          ))}
+        </ul>
+
+        <button className="max-lg:hidden">Login</button>
+
+        <div className="hidden max-lg:block">
+          <img src={hamburger} alt="hamburger" width={25} height={25} />
+        </div>
       </nav>
     </header>
   );
